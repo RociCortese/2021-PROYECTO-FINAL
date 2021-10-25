f74e915738ac2c702e1ca9bc0aa66907a1b7fdb6
RociCortese <rocicortese24@gmail.com>
2021-10-25 17:19:44 -0300

    Home - Perfil  -( Familias/Docente)

--- resources/views/docente.blade.php
+++ resources/views/docente.blade.php
@@ -1,9 +1,10 @@
-@extends('layouts.main', ['activePage' => 'dashboard', 'titlePage' => __('Dashboard')])
+@extends('layouts.main', ['activePage' => 'dashboarddocente', 'titlePage' => __('Dashboard Docente')])
 
 @section('content')
   <div class="content">
     <div class="container-fluid">
-      
+         
+         <h4>HOLA USUARIO DOCENTE</h4>
       
   </div>
   </div>
